edb9ff24f1510b76c021f3665c0688476f4064df
madela1280 <medela1280@gmail.com>
2025-10-17 11:47:47 +0900

    fix: JSX closing div alignment

--- app/components/UnifiedGrid.tsx
+++ app/components/UnifiedGrid.tsx
@@ -699,10 +699,9 @@ const saveRows = async (next: Row[]) => {
                  <div className="text-[11px] text-gray-500 mt-2">
                     * 규칙으로 저장되며 뷰가 재구성됩니다.
                 </div>
-                                       </div>
-                                     )}
-                                 </div>
-                               </div>
+                </div>
+                </div>
+                </div>
        
          {/* 좌측: 필터/검색/다운로드/칼라/오류검사 */}
         <div className="ml-3 flex items-center gap-2">
